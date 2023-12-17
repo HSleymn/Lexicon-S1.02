990ff4fc1c851f83c1a4765e06a1d604f087f070
Celthans <153358552+Kaidoat@users.noreply.github.com>
2023-12-17 16:26:03 +0000

    caca

--- Lexiconpp/talon.cpp
+++ Lexiconpp/talon.cpp
@@ -1,1 +1,1 @@
-
+#include "talon.h"
--- Lexiconpp/talon.h
+++ Lexiconpp/talon.h
@@ -1,2 +1,10 @@
 #ifndef LEXICONPP_TALON_H
 #define LEXICONPP_TALON_H
+
+
+struct Talon {
+    unsigned int capacite;  
+    Carte* tab;		
+    int sommet;			
+
+};

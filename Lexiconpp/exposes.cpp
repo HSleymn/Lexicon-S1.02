6e971df820db0d7088f81109925f39ed82cdff54
Celthans <153358552+Kaidoat@users.noreply.github.com>
2023-12-17 21:35:30 +0000

    struct talon

---
+++ Lexiconpp/exposes.cpp
@@ -0,0 +1,14 @@
+#include "expose.h"
+using namespace std;
+
+
+int main(int argc, char** argv);
+{
+
+
+
+
+
+
+
+}
---
+++ Lexiconpp/exposes.h
@@ -0,0 +1,31 @@
+#ifndef LEXICONPP_TALON_H
+#define LEXICONPP_TALON_H
+
+
+struct  expose{
+    unsigned int capacite;  
+    Carte* tab;		
+    int sommet;			
+
+};
+
+void initialiser(expose& e, unsigned int c );{
+
+
+    
+}
+
+bool estVide(const expose& e);{
+
+}
+
+void empiler(expose& e, const Carte& Ca);{
+
+
+}
+
+void depiler(expose& e){
+
+
+
+}

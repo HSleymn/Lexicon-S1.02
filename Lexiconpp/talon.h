4e80d5b9f169bb2473ab4df732f78b276954a089
Celthans <153358552+Kaidoat@users.noreply.github.com>
2023-12-20 21:15:24 +0000

    caca

--- Lexiconpp/talon.h
+++ Lexiconpp/talon.h
@@ -11,20 +11,36 @@ struct Talon {
 
 void initialiser(Talon& t, unsigned int c );{
 
-
-    
+        for (int i = 0; i < taille; ++i)
+    empiler(t, m[i]);
+        for (int i = 0; i < taille; ++i) {
+     m[i] = sommet(t);
+    depiler(t);
+     }
+    detruire(t);
 }
 
-bool estVide(const Talon& t);{
 
+bool estVide(const Talon& t);{
+    T  = 0;
+    std::stack<T> temp = elements;  // Utilise une copie pour ne pas modifier la pile originale
+        while (!temp.empty()) {
+            somme += temp.top();
+            temp.pop();
 }
 
 void empiler(Talon& t, const Carte& Ca);{
 
-
+    assert(!estPleine(p));
+	t.elems[++t.sommet] = Ca;
+}
 }
 
 void depiler(Talon& t){
+
+    assert(!estVide(t));
+	int v = t.elems[t.sommet--];
+	return v;
 
 
 

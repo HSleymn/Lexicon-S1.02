84850c15c5ee0ddcecc059f4f8e15604730e3adf
HSleymn <noninio30@gmail.com>
2024-01-07 17:07:33 +0100

    Fin du projet

--- main.cpp
+++ main.cpp
@@ -27,12 +27,13 @@ int main(int argc, char** argv)
     //affichage
 
     std::cout << "(Commandes valides : TEPRC)" << std::endl;
-
+    int tourDuJoueurDebut = 0;
+    int tourApresDebut = 0;
     while(joueurs.nbJoueursEnLice > 1){
-        for (int tourDuJoueur = 0; tourDuJoueur < joueurs.nbJoueursTotal; tourDuJoueur++) {
+        for (int tourDuJoueur = tourApresDebut; tourDuJoueur < joueurs.nbJoueursTotal; tourDuJoueur++) {
+            tourApresDebut = 0;
             if(verificationJoueurEnLice(joueurs, joueurs.tabJoueurs[tourDuJoueur])){
                 //affichage des mots
-                afficherTableauMot(tableauDeMot);
                 pointDeControle = ETAT_DE_BASE;
                 //vérification que talon plein, sinon le remplir
                 if (estVide(pileDuTalon)){
@@ -46,6 +47,8 @@ int main(int argc, char** argv)
 
                 }
                 std::cout << std::endl;
+                afficherTableauMot(tableauDeMot);
+
                 //gesttion de l'entrée du joueur
                 while(pointDeControle != ENTREE_VALIDE && pointDeControle != MOT_INVALIDE){
                     std::cout << ">";
@@ -64,6 +67,10 @@ int main(int argc, char** argv)
                 //vérification que le joueur qui vient de jouer a toujours des cartes, si non, terminer le tour
                 if(!verificationJoueurAEncoreDesCartes(joueurs.tabJoueurs[tourDuJoueur])){
                     initialisationNouveauTour(joueurs, jeuDeCartes, tableauDeMot, pileDuTalon, pileCartesExposee);
+                    tourDuJoueurDebut++;
+                    if(tourDuJoueurDebut >= joueurs.nbJoueursTotal){
+                        tourDuJoueurDebut=0;
+                    }
                 }
             }
         }
@@ -74,28 +81,20 @@ int main(int argc, char** argv)
 
 void initialisationNouveauTour(Joueurs& lesJoueurs, jeuDeCarte& jeuDeCartes, TabMots& tabMots, Pile& pileDuTalon, Pile& pileCartesExposees){
     ajoutPointsFinDeTour(lesJoueurs);
-    std::cout <<"caca 1 ";
-
     std::cout << "Le tour est fini" << std::endl << "* Scores" << std::endl;
     for(int compteurJoueur=0; compteurJoueur < lesJoueurs.nbJoueursTotal ; compteurJoueur++) {
         if(verificationJoueurEnLice(lesJoueurs, lesJoueurs.tabJoueurs[compteurJoueur]))
             std::cout << "Joueur " << lesJoueurs.tabJoueurs[compteurJoueur].idJoueur << " : " << lesJoueurs.tabJoueurs[compteurJoueur].nombreDePoints << std::endl;
     }
-    std::cout <<"caca 2 ";
     for(int compteur=0; compteur<lesJoueurs.nbJoueursTotal; compteur++){
         if(lesJoueurs.tabJoueurs[compteur].nombreDePoints > 100){
             eliminationJoueur(lesJoueurs, lesJoueurs.tabJoueurs[compteur]);
         }
     }
-    std::cout <<"caca 3";
 
     detruire(pileDuTalon);
-    std::cout <<"caca 4 ";
     detruire(pileCartesExposees);
-    std::cout <<"caca 5 ";
-
     initialiserJeuDeCarte(jeuDeCartes);
-    std::cout <<"caca 7 ";
     unsigned int indiceCarte=0;
     for(int compteur=0; compteur < lesJoueurs.nbJoueursTotal; compteur++){
         if(verificationJoueurEnLice(lesJoueurs, lesJoueurs.tabJoueurs[compteur])){
@@ -103,17 +102,11 @@ void initialisationNouveauTour(Joueurs& lesJoueurs, jeuDeCarte& jeuDeCartes, Tab
             indiceCarte++;
         }
     }
-    std::cout <<"caca 8 ";
     recupererJeuDeCarteRestant(jeuDeCartes, lesJoueurs.nbJoueursEnLice);
-    std::cout <<"caca 9 ";
     initialiser(pileDuTalon, jeuDeCartes.capacite);
-    std::cout <<"caca 10 ";
     initialiser(pileCartesExposees, jeuDeCartes.capacite);
-    std::cout <<"caca 11 ";
     remplirTalonEtExposee(pileDuTalon, pileCartesExposees, jeuDeCartes);
-    std::cout <<"caca 12 ";
     initialiserTableauDeMot(tabMots, TAILLE_TABLEAU , PAS_EXTENSTION_TABLEAU_MOT);
-    std::cout <<"caca 13 ";
 
 }
 
